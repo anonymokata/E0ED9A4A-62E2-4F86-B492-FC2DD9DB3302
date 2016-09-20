d30e48cbf708c957d97337190911d699ff0c6d19
anonymous <anonymous>
2016-09-20 00:00:41 -0400

    Wrote code for arabic_to_roman, all tests pass
    Running suite(s): arabic_to_roman
    100%: Checks: 28, Failures: 0, Errors: 0

--- src/roman.c
+++ src/roman.c
@@ -4,7 +4,57 @@
 #include <errno.h>
 #include "roman.h"
 
+/* map arabic numerals to roman numerals */
+struct map_elem {
+	arabic_t arabic;
+	roman_t roman;
+};
+
+struct map_elem rmap[] = {
+	{1, "I"},
+	{4, "IV"},
+	{5, "V"},
+	{9, "IX"},
+	{10, "X"},
+	{40, "XL"},
+	{50, "L"},
+	{90, "XC"},
+	{100, "C"},
+	{400, "CD"},
+	{500, "D"},
+	{900, "CM"},
+	{1000, "M"},
+};
+#define MAP_ELEM_LENGTH 13
+
+// Convert arabic numeral a to roman numeral stored in r.
+// Zero returned on success, negative value on error.
 int arabic_to_roman(arabic_t a, roman_t* r)
 {
+	if (1 > a || MAX_ARABIC < a) // arabic out of accepted range
+		return -1;
+
+	// Allocate vector for roman numerals
+	*r = calloc((MAX_ROMAN_LENGTH+1),sizeof(char)); //+1 for '\0'
+	if (NULL == *r)
+		return errno;
+	int idx = MAP_ELEM_LENGTH-1; //last index in mapped roman numeral list
+
+	// Build roman numeral
+	do {
+		struct map_elem v = rmap[idx];
+		if (a >= v.arabic) {
+			strcat((char*)*r, v.roman);
+			a = a - v.arabic;
+		} else {
+			idx--;
+		}
+	} while ((0 < a) && (0 <= idx));
+
+	if (0 != a) {
+		free(r);
+		return -1;
+	}
+
 	return 0;
 }
--- src/roman.h
+++ src/roman.h
@@ -1,8 +1,11 @@
 #ifndef roman_h_
 #define roman_h_
 
+#define MAX_ARABIC 3999
+#define MAX_ROMAN_LENGTH 9
+
 typedef char* roman_t;
-typedef int arabic_t;
+typedef uint arabic_t;
 
 int arabic_to_roman(arabic_t a, roman_t* r);
 
