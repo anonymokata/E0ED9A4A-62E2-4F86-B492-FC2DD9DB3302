e540d24734105a92ecd52790510a00ab75cb2a6f
anonymous <anonymous>
2016-09-20 00:10:02 -0400

    Added first test for roman_to_arabic
    Fails to compile, as roman_to_arabic is not defined yet.

--- tests/roman2arabic.c
+++ tests/roman2arabic.c
@@ -1,6 +1,14 @@
 #include <stdlib.h>
 #include <check.h>
 
+START_TEST(r2a_1)
+{
+	roman_t r = "I";
+	int ret;
+	ret = roman_to_arabic(r);
+	ck_assert_int_eq(ret, 1);
+}
+END_TEST
 
 /* Tests for roman_to_arabic to suite */
 Suite* check_r2a(void)
@@ -10,6 +18,8 @@ Suite* check_r2a(void)
 
 	s = suite_create("roman_to_arabic");
 	tc = tcase_create("r2a");
+
+	tcase_add_test(tc, r2a_1);
 
 	suite_add_tcase(s, tc);
 	return s;
